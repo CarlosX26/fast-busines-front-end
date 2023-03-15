c1dbbecaf9172791ebf0eea203347bd6626f02e4
CarlosX26 <cjunior1@live.com>
2023-03-15 19:40:46 -0300

    feat: add params in routing for retrieve product by id

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  productId!: number;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.productId = Number(params["id"]);
+    });
+    console.log(this.productId);
+  }
 }
